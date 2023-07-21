c8c51f95175058da2144e2336c19aa308121202e
Hugh Ratsch <hugh.ratsch.18@gmail.com>
2023-07-21 13:50:54 -0500

    updated project to add App.js and other components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+const App = () => {
+    return ( 
+        <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList robots={robots}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+     );
 }
-
+ 
 export default App;
