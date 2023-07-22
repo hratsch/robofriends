c375a1493b0602541032a6e544608d7c56f86fd7
Hugh Ratsch <hugh.ratsch.18@gmail.com>
2023-07-22 13:02:27 -0500

    added state to interact with app via searchbox

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from 'react'
-import { robots } from './robots';
+import React, { Component } from 'react'
 import SearchBox from './SearchBox';
 import CardList from './CardList';
+import { robots } from './robots'
 
-const App = () => {
-    return ( 
+// changing to react component class
+
+// const App = () => {
+//     return ( 
+//         <div className='tc'>
+//             <h1>RoboFriends</h1>
+//             <SearchBox />
+//             <CardList robots={robots}/>
+//         </div>
+//      );
+// }
+ 
+// export default App;
+
+
+class App extends Component { 
+    constructor () {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() { 
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
         <div className='tc'>
             <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-     );
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>
+        </div>  
+        );
+    }
 }
  
 export default App;
